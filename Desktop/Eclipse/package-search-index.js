0ac3afef8c1e117edfe08b3d44ca31960e1b0769
Joao <joao2020270344@gmail.com>
2021-12-03 14:26:26 -0300

    Tudo da Allura

---
+++ Desktop/Eclipse/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"arrays"},{"l":"aula_1"},{"l":"aula_3"},{"l":"diferencas_entre_Variaveis"},{"l":"financeiro"},{"l":"ManipulacaoString"},{"l":"metodos"},{"l":"modelo"},{"l":"operadores"},{"l":"stringBuilder"},{"l":"Subpacote"}];updateSearchResults();
